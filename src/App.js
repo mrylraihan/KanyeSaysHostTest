619487d515bf565da57211b2de7f36d99e6d50f0
mrylraihan <mr.ylraihan@gmail.com>
2023-03-01 21:09:13 -0500

    kanye says button and fetch call

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [quote, setQuote]= useState('')
+
+  const getQuote = () =>{
+    fetch('https://api.kanye.rest/')
+    .then(res=>res.json())
+    .then(result=>setQuote(result.quote))
+    .catch(console.error)
+  }
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {quote && <p>{quote}</p>}
+       <button onClick={getQuote}>What does Kanye say</button>
       </header>
     </div>
   );
